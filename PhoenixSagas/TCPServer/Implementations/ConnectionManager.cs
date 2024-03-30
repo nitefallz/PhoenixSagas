4580fcfb61dbec894106fd5eceade48fd187782a
nightfall <nitefallz@gmail.com>
2024-03-29 23:51:24 -0400

    working branch no

--- PhoenixSagas/TCPServer/Implementations/ConnectionManager.cs
+++ PhoenixSagas/TCPServer/Implementations/ConnectionManager.cs
@@ -19,15 +19,16 @@ namespace PhoenixSagas.TCPServer.Implementations
         private readonly IKafkaConsumer<PlayerOutput> _kafkaOutputConsumer;
         private readonly ILogger<OutputHandler> _outputHandlerLogger;
 
-        public ConnectionManager(IConnectedClientsMap clients, IKafkaProducer<PlayerInput> kafkaInputProducer, string playerOutputTopic, ILogger<OutputHandler> outputHandlerLogger)
+
+        public ConnectionManager(IConnectedClientsMap clients, ILogger<OutputHandler> outputHandlerLogger)
         {
             _clients = clients ?? throw new ArgumentNullException(nameof(clients));
-            _kafkaInputProducer = kafkaInputProducer ?? throw new ArgumentNullException(nameof(kafkaInputProducer));
+            _kafkaInputProducer = new KafkaFactory().BuildProducer<PlayerInput>("PlayerInput");
             _outputHandlerLogger = outputHandlerLogger ?? throw new ArgumentNullException(nameof(outputHandlerLogger));
 
             var outputHandler = new OutputHandler(clients, _outputHandlerLogger);
-            _kafkaOutputConsumer = new KafkaConsumer<PlayerOutput>(playerOutputTopic, outputHandler);
-            _kafkaOutputConsumer.Start();
+           // _kafkaOutputConsumer = new KafkaConsumer<PlayerOutput>(playerOutputTopic, outputHandler);
+           // _kafkaOutputConsumer.Start();
         }
 
         public void HandleNewConnection(Socket socket)
